902e7153ccfae87df46f6ba0968845059d8e54a2
siniii <01089659795a@gmail.com>
2026-07-29 17:17:31 +0900

    JAVA 6일차_Ex12_연습예제.java

---
+++ java_ksi/src/day06/Ex12_연습예제.java
@@ -0,0 +1,32 @@
+package day06;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex12_연습예제 {
+
+	public static void main(String[] args) {
+		// ArrayList를 이용하여 정수를 저장하는 list를 생성
+		ArrayList<Integer>list = new ArrayList<Integer>();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		// list에 숫자를 3개 입력하여 저장
+		for( ; list.size() < 3; ) {
+			System.out.println("정수 입력 : ");
+			int num = scan.nextInt();
+			list.add(num);
+		}
+		
+		/*
+		 * list.add(1);
+		 * list.add(2);
+		 * list.add(3);
+		 */
+		
+		// list에 있는 값을 콘솔에 출력
+		System.out.println(list);
+
+	}
+
+}
