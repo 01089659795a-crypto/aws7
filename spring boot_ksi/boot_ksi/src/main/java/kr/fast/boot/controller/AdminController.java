8f7c9f8098253098a60955b47de4c0b5986d8aea
siniii <01089659795a@gmail.com>
2026-08-21 18:05:57 +0900

    Update AdminController.java

--- spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/controller/AdminController.java
+++ spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/controller/AdminController.java
@@ -23,68 +23,62 @@ public class AdminController {
 	
 	private final BoardService boardService;
 	
-	// url : /api/admn/board
-	// method : post를 처리하는 메서드를 추가
-	// 리턴 : "OK" 문자열을 리턴
+	// url : /api/admin/board
+	// method : post를 처리하는 메서드를 추가 
+	// 리턴 : "OK" 문자열을 리턴 
 	@PostMapping("/board")
-	public ResponseEntity<String> boardPost(@RequestBody BoardDTO dto) {
-		//게시판서비스야, 새 게시판 등록해줘. 새 게시판명 여기있어. 등록하고 결과를 알려줘
+	public ResponseEntity<String> boardPost(@RequestBody BoardDTO dto){
+		//게시판 서비스야. 새 게시판 등록해줘. 여기 새 게시판명이 있어. 등록하고 결과를 알려줘
 		try {
 			boolean isInsert = boardService.insertBoard(dto);
-			return ResponseEntity.ok("게시판을 등록했습니다.");
+			return ResponseEntity.ok("게시판을 등록했습니다.");			
 		}catch (Exception e) {
 			return ResponseEntity.ok(e.getMessage());
+		}
 		
 	}
-
-}
-
+	
 	//url : /api/admin/board
 	//method : put
 	//리턴 : "OK"
 	@PutMapping("/board")
-	public ResponseEntity<String> boardPut(@RequestBody BoardDTO dto) {
+	public ResponseEntity<String> boardPut(@RequestBody BoardDTO dto){
 		try {
-			//서비스야. 게시판번호와 게시판 이름을 줄게. 게시판 명을 수정해줘
+			//서비스야. 게시판번호와 이름을 줄게. 게시판명를 수정해줘.
 			boardService.updateBoard(dto);
-			return ResponseEntity.ok("게시판을 수정했습니다.");
+			return ResponseEntity.ok("게시판을 수정했습니다.");			
 		}catch(Exception e) {
-			return ResponseEntity.ok(e.getMessage());
+			return ResponseEntity.ok(e.getMessage());			
 		}
-
+		
+		
 	}
 	
 	@GetMapping("/board")
-	public ResponseEntity<List<Board>> boardGet() {
-		//서비스야. 게시판목록을 가져와줘
+	public ResponseEntity<List<Board>> boardGet(){
+		//서비스야. 게시판목록을 가져와줘 
 		List<Board> list = boardService.getBoardList();
-		return ResponseEntity.ok(list);
+		return ResponseEntity.ok(list);			
 	}
 	
-	//메서드를 추가해서 화면에 ok가 출력되도록 작업
-	
+
 	@DeleteMapping("/board")
-	public ResponseEntity<String> boardDelete(@RequestBody BoardDTO dto) {
-		//서비스야, 게시판 번호 줄게 게시판 삭제해줘
-		//서비스야.게시판삭제해(게시판번호);
+	public ResponseEntity<String> boardDelete(@RequestBody BoardDTO dto){
+		//서비스야. 게시판 번호 줄게 게시판 삭제해줘 
+		//서비스.게시판삭제해(게시판번호);
 		try {
-			boardService.deleteBoard(dto.id());			
-			return ResponseEntity.ok("게시판을 삭제했습니다.");
+			boardService.deleteBoard(dto.id());
+			return ResponseEntity.ok("게시판을 삭제했습니다.");			
 		}catch(Exception e) {
-			return ResponseEntity.ok("OK");
+			return ResponseEntity.ok(e.getMessage());
 		}
 	}
 }
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
+
+
+
+
+
+
+
