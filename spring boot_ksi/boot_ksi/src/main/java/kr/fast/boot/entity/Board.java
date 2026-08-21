7ba8aeff94dfded452d0003cfc34fc3e4119da33
siniii <01089659795a@gmail.com>
2026-08-21 18:06:09 +0900

    Update Board.java

--- spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/entity/Board.java
+++ spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/entity/Board.java
@@ -1,6 +1,5 @@
 package kr.fast.boot.entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,12 +12,11 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "board")
-@Getter
+@Getter 
 @NoArgsConstructor
 @AllArgsConstructor
 public class Board {
 	
-
 	@Id
 	//기본키 생성을 DB가 생성함. 보통 기본키가 auto_increment일 때 사용
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,12 +24,15 @@ public class Board {
 	
 	@Column(nullable = false)
 	private String name;
-	
+
 	public Board(String name) {
 		this.name = name;
 	}
-	
+
 	public void update(String name) {
 		this.name = name;
 	}
 }
+
+
+
