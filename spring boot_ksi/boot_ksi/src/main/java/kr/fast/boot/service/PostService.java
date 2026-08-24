66eca87a8121203d4ed00c2b8fa74b8bbfe37849
siniii <01089659795a@gmail.com>
2026-08-24 17:37:25 +0900

    Update PostService.java

--- spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/service/PostService.java
+++ spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/service/PostService.java
@@ -3,6 +3,7 @@ package kr.fast.boot.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.glassfish.jaxb.core.v2.runtime.IllegalAnnotationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,21 +42,67 @@ public class PostService {
 		//레포야 엔티티 줄게 저장해.
 		postRepository.save(post);
 	}
-
+	@Transactional
 	public List<Post> getPostList() {
 		//내림차순 => 최신 게시글이 제일 처음
-		List<Post> list = postRepository.findAllByOrderByIdDesc();
+		List<Post> list = postRepository.findAllByIsDeletedOrderByIdDesc("N");
 		//List<Post> list = postRepository.findAll(); //오름차순 => 최신 게시글이 제일 마지막
 		
 		
 		return list; 
 	}
-
+	@Transactional
 	public Post getPost(int id) {
 		Post post
 		= postRepository.findById(id)
 			.orElseThrow(()-> new IllegalArgumentException("게시글이 존재하지 않습니다."));
 		return post;
+	}
+	@Transactional
+	public void updateView(int id) {
+		//게시글 엔티티를 가져옴
+		Post post = getPost(id);
+		//조회수 증가
+		post.updateView();
+		
+	}
+	@Transactional
+	public void deletePost(int id) {
+		//id와 일치하는 게시글을 가져오는데 없으면 예외 발생
+		//레포야 게시글 가져와 id 줄게. 단, 없으면 예외 발생 시켜
+		Post post
+		= postRepository.findById(id)
+			.orElseThrow(()-> new IllegalArgumentException("삭제할 게시글이 없습니다."));
+		
+		//이미 삭제된 게시글이면 예외를 발생
+		if(post.getIsDeleted().equals("Y")) {
+			throw new IllegalArgumentException("이미 삭제된 게시글입니다.");
+		}
+		
+		//해당 게시글을 삭제
+		//레포야 게시글 삭제해줘. 게시글 줄게
+		
+		//soft삭제 : 실제 데이터 안 지움
+		post.delete();
+		
+		//직접 데이터 삭제(hard삭제) : 삭제된 게시글이 왜 삭제됐는지 확인할 수 없음
+		//postRepository.delete(post);
+	}
+	@Transactional
+	public void updatePost(int id, PostDTO dto) {
+		//id와 일치하는 게시글을 가져옴
+		Post post = postRepository.findById(id)
+				.orElseThrow(()->new IllegalArgumentException("등록되지 않은 게시글입니다."));
+		//수정할 제목과 내용 체크
+		if(dto == null || !dto.checkContentValid()) {
+			throw new IllegalArgumentException("제목을 입력하세요.");
+		}
+		if(!dto.checkContentValid()) {
+			throw new IllegalArgumentException("내용을 입력하세요");
+		}
+		//게시글이 제목과 내용을 수정
+		post.update(dto.title(), dto.content());
+		
 	}
 	
 	
