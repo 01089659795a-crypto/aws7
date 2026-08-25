e43f10b112c6d3df9006d9c79e0d38d8672b4a22
siniii <01089659795a@gmail.com>
2026-08-25 12:32:57 +0900

    Create AuthController.java

---
+++ spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/controller/AuthController.java
@@ -0,0 +1,44 @@
+package kr.fast.boot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.fast.boot.dto.LoginDTO;
+import kr.fast.boot.dto.TokenDTO;
+import kr.fast.boot.service.AuthService;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@RequestMapping("/api/auth")
+@Log4j2
+@AllArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<Object> loginPost(@RequestBody LoginDTO dto){
+		log.info("로그인 중입니다.");
+		try {
+			TokenDTO tokenDto = authService.login(dto);
+			return ResponseEntity.ok(tokenDto);
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+		}
+	}
+	@GetMapping("/me")
+	public ResponseEntity<Object> meGet(){
+		log.info("회원 정보를 가져오는 중입니다.");
+		try {
+			return ResponseEntity.ok("OK");
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+		}
+	}
+}
