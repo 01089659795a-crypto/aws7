e460e4452b59eede583a971b3530668c91f3fa32
siniii <01089659795a@gmail.com>
2026-08-26 15:22:05 +0900

    Update AuthService.java

--- spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/service/AuthService.java
+++ spring boot_ksi/boot_ksi/src/main/java/kr/fast/boot/service/AuthService.java
@@ -1,5 +1,6 @@
 package kr.fast.boot.service;
 
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,8 +37,57 @@ public class AuthService {
 			throw new IllegalArgumentException("아이디나 비번이 일치하지 않습니다.");
 		}
 		//같으면 토큰을 생성
-		String token = jwtProvider.createToken(user.getId(), user.getRole());
-		return new TokenDTO("accessToken", token);
+		String accessToken = jwtProvider.createToken(user.getId(), user.getRole());
+		
+		String refreshToken = jwtProvider.createRefreshToken(user.getId());
+		//리프레쉬토큰을 저장할 쿠키 생성
+		ResponseCookie refreshCookie
+			= createCookie(
+					"refreshToken",
+					refreshToken,
+					7 * 24 * 60 * 60,
+					"/api/auth/refresh");
+//				ResponseCookie.from("refreshToken", refreshToken)
+//				.httpOnly(true) // js 접근 불가
+//				.secure(false) //HTTP에서도 전송되도록
+//				.sameSite("Lax") //다른 사이트에서 요청시 전송 제한
+//				.path("/api/auth/refresh") //해당 URL 요청시에만 쿠키 전송
+//				.maxAge(7 * 24 * 60 * 60) //쿠키 유요 기간 : 7일
+//				.build();
+		return new TokenDTO(accessToken, refreshCookie);
+	}
+
+	public String createNewAccessToken(String refreshToken) {
+		//리프레쉬토큰이 맞는지 확인
+		if(!jwtProvider.isRefreshToken(refreshToken)) {
+			throw new IllegalArgumentException("유효하지 않은 리프레쉬 토큰입니다.");
+		}
+		//유효한 리프레쉬토큰인지 확인
+		if(!jwtProvider.validateToken(refreshToken)) {
+			throw new IllegalArgumentException("만료된 리프레쉬 토큰입니다.");
+		}
+		//리프레쉬 토큰에서 id를 가져옴. 왜? 어세스 토큰 만들 때 아이디와 권한이 필요
+		String username = jwtProvider.getUsername(refreshToken);
+		//db에서 회원 정보를 가져옴. 왜? 리프레쉬 토큰에 role을 추가 안해서
+		Member user = memberRepository.findById(username)
+				.orElseThrow(()->new IllegalArgumentException("등록되지 않은 회원입니다."));
+		String role = user.getRole();
+		//새 어세스 토큰을 생성
+		String accessToken = jwtProvider.createToken(username, role);
+		//생성한 어세스 토큰을 리턴
+		return accessToken;
+	}
+
+	public ResponseCookie createCookie(
+			String cookieName, String value, int maxAge, String path) {
+		
+		return ResponseCookie.from(cookieName, value)
+				.httpOnly(true) // js 접근 불가
+				.secure(false) //HTTP에서도 전송되도록
+				.sameSite("Lax") //다른 사이트에서 요청시 전송 제한
+				.path(path) //해당 URL 요청시에만 쿠키 전송
+				.maxAge(maxAge) //쿠키 유요 기간 : 7일
+				.build();
 	}
 
 }
