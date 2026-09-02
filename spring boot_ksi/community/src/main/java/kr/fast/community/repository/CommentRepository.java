f340de77d9986ffb4bab23cff7694699185461da
siniii <01089659795a@gmail.com>
2026-09-02 15:12:31 +0900

    스프링부트 13일차 댓글 목록 페이징 적용 완료

--- spring boot_ksi/community/src/main/java/kr/fast/community/repository/CommentRepository.java
+++ spring boot_ksi/community/src/main/java/kr/fast/community/repository/CommentRepository.java
@@ -2,12 +2,15 @@ package kr.fast.community.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.fast.community.entity.Comment;
 import kr.fast.community.entity.File;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+	Page<Comment> findAllByPostId(int postId, Pageable pageable);
 	
-	List<Comment> findAllByPostId(int postId);
 }
--- spring boot_ksi/community/src/main/java/kr/fast/community/service/PostService.java
+++ spring boot_ksi/community/src/main/java/kr/fast/community/service/PostService.java
@@ -162,15 +162,16 @@ public class PostService {
 		return new MessageResponse(true, "댓글을 등록했습니다.");
 	}
 
-	public List<Comment> getComments(int postId) {
+	public PageResponse<Comment> getComments(int postId, Pageable pageable) {
 		// 게시글이 있는지 확인
 		Post post = postRepository.findByIdAndIsDeleted(postId, "N");
-		System.out.println(post);
+		
 		if(post == null) {
 			throw new RuntimeException("존재하지 않은 게시글입니다.");
 		}
 		
-		return commentRepository.findAllByPostId(postId); //포스트기준으로 모든 댓글 가져와
+		Page<Comment>page = commentRepository.findAllByPostId(postId, pageable); //포스트기준으로 모든 댓글 가져와
+		return new PageResponse<Comment>(page, 3);
 	}
 	
 }
